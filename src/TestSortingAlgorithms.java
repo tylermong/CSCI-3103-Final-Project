e265276e5ad86a9c426f2edfae67c566ae77e60d
tyler.mong <117101211+tylermong@users.noreply.github.com>
2023-12-12 23:57:11 -0500

    fixed comment

--- src/TestSortingAlgorithms.java
+++ src/TestSortingAlgorithms.java
@@ -1,3 +1,11 @@
+/**
+ * Calls the testAlgorithms method in the SortingAlgorithms class.
+ *
+ * @author Tyler Mong
+ * @version 1.1
+ * @since 2023-12-12
+ */
+
 public class TestSortingAlgorithms
 {
     public static void main(String[] args)
